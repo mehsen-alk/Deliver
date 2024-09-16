d54005167b04db0359bbb4881aa990c92707de3f
Moshen <mehsen2222@gmail.com>
2024-09-16 15:07:44 +0300

    add base response

---
+++ src/Core/Deliver.Application/Responses/BaseResponse.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Deliver.Application.Responses
+{
+    public class BaseResponse
+    {
+        private BaseResponse()
+        {
+            Message = string.Empty;
+            StatusCode = 0;
+        }
+
+        public BaseResponse(int statusCode, string message)
+        {
+            Message = message;
+            StatusCode = statusCode;
+        }
+
+        public BaseResponse(int statusCode, string message, object data)
+        {
+            Message = message;
+            StatusCode = statusCode;
+            Data = data;
+        }
+
+        public static BaseResponse FetchSuccessfully(int statusCode = 200, string message = "fetched successfully", object? data = null)
+        {
+            return new BaseResponse
+            {
+                StatusCode = statusCode,
+                Message = message,
+                Data = data,
+            };
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public object? Data { get; set; }
+    }
+}

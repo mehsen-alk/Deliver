aece827dea252c547f1acecaa00f04b8161b2797
Moshen <mehsen2222@gmail.com>
2024-09-20 14:02:37 +0300

    add Authorized health routes

--- src/API/Deliver.Api/Controller/healthController.cs
+++ src/API/Deliver.Api/Controller/healthController.cs
@@ -1,4 +1,5 @@
 using Deliver.Application.Responses;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Deliver.Api.Controllers
@@ -15,6 +16,22 @@ namespace Deliver.Api.Controllers
         [HttpGet("")]
         [ProducesResponseType(typeof(BaseResponse<string>), StatusCodes.Status200OK)]
         public ActionResult CheckServerHealth()
+        {
+            return Ok(BaseResponse<string>.FetchSuccessfully(data: "server is up and running"));
+        }
+
+        [HttpGet("/driver")]
+        [Authorize(Roles = "Driver")]
+        [ProducesResponseType(typeof(BaseResponse<string>), StatusCodes.Status200OK)]
+        public ActionResult CheckServerHealthDriver()
+        {
+            return Ok(BaseResponse<string>.FetchSuccessfully(data: "server is up and running"));
+        }
+
+        [HttpGet("/rider")]
+        [Authorize(Roles = "Rider")]
+        [ProducesResponseType(typeof(BaseResponse<string>), StatusCodes.Status200OK)]
+        public ActionResult CheckServerHealthRider()
         {
             return Ok(BaseResponse<string>.FetchSuccessfully(data: "server is up and running"));
         }

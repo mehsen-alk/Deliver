7b830b6624e3ce4f79fd66a7e350a1bb3e1a0b37
Moshen <mehsen2222@gmail.com>
2024-09-27 20:33:27 +0300

    improve NotFoundException

--- src/Core/Deliver.Application/Exceptions/NotFoundException.cs
+++ src/Core/Deliver.Application/Exceptions/NotFoundException.cs
@@ -2,9 +2,19 @@
 {
     public class NotFoundException : ApplicationException
     {
+        public NotFoundException()
+            : base("not found.")
+        {
+        }
+        public NotFoundException(string message)
+            : base(message)
+        {
+        }
         public NotFoundException(string name, object key)
             : base($"{name} ({key}) is not found")
         {
         }
+
+
     }
 }

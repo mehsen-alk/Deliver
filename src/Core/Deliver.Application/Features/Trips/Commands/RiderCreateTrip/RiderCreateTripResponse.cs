c4c12b7ab0b5aac79b8c52bb72a9423084d83121
Moshen <mehsen2222@gmail.com>
2024-11-29 14:24:42 +0300

    improve CQRS structure in rider create trip flow

--- src/Core/Deliver.Application/Features/Trips/Commands/RiderCreateTrip/RiderCreateTripResponse.cs
+++
@@ -1,7 +0,0 @@
-using Deliver.Application.Responses;
-
-namespace Deliver.Application.Features.Trips.Commands.RiderCreateTrip;
-
-public class RiderCreateTripResponse : BaseResponse<RiderCreateTripDto>
-{
-}

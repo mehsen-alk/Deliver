961b545051ee723ed151016ec4ec0ec691244c36
Moshen <mehsen2222@gmail.com>
2025-03-03 14:37:15 +0300

    remove unused fields in EditProfileByRiderRequest

--- src/Core/Deliver.Application/Features/Profiles/RiderProfile/Commands/EditProfileByRider/EditProfileByRiderRequest.cs
+++ src/Core/Deliver.Application/Features/Profiles/RiderProfile/Commands/EditProfileByRider/EditProfileByRiderRequest.cs
@@ -4,7 +4,5 @@ public class EditProfileByRiderRequest
 {
     public required string Name { get; set; } = string.Empty;
     public required string Phone { get; set; } = string.Empty;
-    public string? VehicleImage { get; set; } = null;
-    public string? LicenseImage { get; set; } = null;
     public string? ProfileImage { get; set; } = null;
 }

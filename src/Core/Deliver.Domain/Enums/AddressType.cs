1ffd5659a26fdcdd993c35bc542b78a459ff2d4c
Mohsen <mehsen2222@gmail.com>
2025-09-14 21:08:28 +0300

    add more AddressType

--- src/Core/Deliver.Domain/Enums/AddressType.cs
+++ src/Core/Deliver.Domain/Enums/AddressType.cs
@@ -4,5 +4,7 @@ public enum AddressType
 {
     PickUp,
     DropOff,
-    DriverLocationWhenAcceptOrder
+    DriverLocationWhenAcceptOrder,
+    DriverLocationWhenArriveToPickUp,
+    DriverLocationWhenArriveToDropOff
 }
